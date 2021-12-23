1fbca1dff225e8e2a05c81189d75d03092e34421
kobrakobrakobra <david@ozmo.hu>
2021-12-23 21:18:56 +0100

    css bugfix

--- src/components/Bingo.vue
+++ src/components/Bingo.vue
@@ -88,6 +88,7 @@ export default {
 
 .bingo-card {
   margin: 24px 0;
+  padding: 8px 16px;
   box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1), 0px 4px 6px -4px rgba(0, 0, 0, 0.1);
   height: 72px;
   border-radius: 24px;
